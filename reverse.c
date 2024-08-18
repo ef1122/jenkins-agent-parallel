c844cc359c2404e96cb6d2151214a42fdf7b0cd8
Apasoft Training <54261225+ApasoftTraining@users.noreply.github.com>
2024-08-18 21:54:32 +0200

    update

--- reverse.c
+++ reverse.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <string.h>                             
-void reverse(char* str) {
-      int n = strlen(str);
-      for (int i = 0; i < n / 2; i++) {
-         char temp = str[i];
-         str[i] = str[n - i - 1];
-         str[n - i - 1] = temp;
-       }
+void reverse_string(char *str) {
+    int n = strlen(str);
+    for (int i = 0; i < n / 2; i++) {
+        char temp = str[i];
+        str[i] = str[n - i - 1];
+        str[n - i - 1] = temp;
     }
+}
+
 int main(int argc, char *argv[]) {
-   char text=reverse(argv[1]);
-   printf("Reversed: %s\\n", text);
-   return 0;
+    if (argc != 2) {
+        printf("Usage: %s <text>\n", argv[0]);
+        return 1;
+    }
+
+    reverse_string(argv[1]);
+
+    printf("Reverse text: %s\n", argv[1]);
+
+    return 0;
 }
--- uppercase.c
+++ uppercase.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <ctype.h>
                   
-int main(int argc, char *argv[]) {
-    char text[];
-    for(int i = 0; argv[1][i]; i++){
-        text[i] = toupper(text[i]);
+void to_uppercase(char *str) {
+    while (*str) {
+        *str = toupper(*str);
+        str++;
     }
-    printf("Uppercase: %s\\n", text);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <text>\n", argv[0]);
+        return 1;
+    }
+
+    to_uppercase(argv[1]);
+
+    printf("Text in uppercase: %s\n", argv[1]);
+
     return 0;
 }
